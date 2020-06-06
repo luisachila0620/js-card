5174c17e678ae211a0c3978a6fbbdf93dc0ec15f
luisachila0620 <luisarchila038@gmail.com>
2020-06-06 01:07:37 +0000

    js card

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,16 +4,24 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db
 from api.utils import generate_sitemap
+import random
 #from models import Person
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/card', methods=['GET'])
+def get_card():
 
-    response_body = {
-        "hello": "world"
+    cards = ['♡', '♢', '♤', '♧']
+    values = ["ace","king","queen","jacks""10","9","8","7","6","5","4","3","2"]
+
+    random_cards_position = random.randint(0,5)
+    random_values_position = random.randint(0,10)
+
+    body = {
+        "cards":cards[random_cards_position],
+        "number":values[random_values_position]
     }
 
-    return jsonify(response_body), 200
+    return jsonify(body), 200
